11ea88ff840d0f57bc748987d9abc371f1351abe
isudzumi <isudzumi@gmail.com>
2018-08-28 19:52:44 +0900

    Update

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,3 @@
 import "../css/main.css";
-import "../../node_modules/css-regions-polyfill/bin/css-regions-polyfill.js";
+import "../../node_modules/css-regions-polyfill/bin/css-regions-polyfill.min.js";
+// import "../../node_modules/css-polyfill/bin/css-polyfills.min.js";
