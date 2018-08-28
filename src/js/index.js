ce65b382b9a3b5cd0c4f935cf9daebfde872fdbc
isudzumi <isudzumi@gmail.com>
2018-08-29 08:54:54 +0900

    Add pagedjs

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,12 @@
 import "../css/main.css";
-import "../../node_modules/css-regions-polyfill/bin/css-regions-polyfill.min.js";
+// import "../../node_modules/css-regions-polyfill/bin/css-regions-polyfill.min.js";
 // import "../../node_modules/css-polyfill/bin/css-polyfills.min.js";
+
+/**
+import { Previewer } from 'pagedjs';
+
+let paged = new Previewer();
+let flow = paged.preview(DOMContent, ["../css/main.css"], document.body).then((flow) => {
+    console.log("Rendered", flow.total, "pages.");
+})
+*/
